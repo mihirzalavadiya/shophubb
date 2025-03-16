e034f8c9726735d6fbdf445a8080ee2a352d8363
mihirzalavadiya <mihirzalavadiya@gmail.com>
2025-03-16 06:05:51 +0530

    create product details page

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,4 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'ShopHubb is a platform that helps you find the best deals on the products you love.';
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
